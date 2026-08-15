db43741f963b29c95639eeb4afccfe226724da69
屑黄某 <391108721@qq.com>
2026-08-15 16:49:43 +0800

    Update main.lua library

--- main.lua
+++ main.lua
@@ -211,6 +211,7 @@ local Library = (function()
 		local GreenBox = nil
 		local BlueBox = nil
 		local activeSlider = nil
+		local curInput = nil
 		local function CurrentColor()
 			return Color3.fromHSV(hue, sat, vib)
 		end
@@ -268,6 +269,7 @@ local Library = (function()
 			GreenBox = nil
 			BlueBox = nil
 			activeSlider = nil
+			curInput = nil
 			if OutsideConn then
 				OutsideConn:Disconnect()
 				OutsideConn = nil
@@ -404,18 +406,26 @@ local Library = (function()
 			end
 			SatVibMap.InputBegan:Connect(function(input)
 				if IsPress(input) then
+					if curInput and curInput ~= input then
+						return
+					end
+					curInput = input
 					activeSlider = "SatVib"
 					UpdateSatVib(UserInputService:GetMouseLocation().X, UserInputService:GetMouseLocation().Y)
 				end
 			end)
 			HueBar.InputBegan:Connect(function(input)
 				if IsPress(input) then
+					if curInput and curInput ~= input then
+						return
+					end
+					curInput = input
 					activeSlider = "Hue"
 					UpdateHue(UserInputService:GetMouseLocation().Y)
 				end
 			end)
 			UserInputService.InputChanged:Connect(function(input)
-				if activeSlider then
+				if curInput and input == curInput and activeSlider then
 					if activeSlider == "SatVib" then
 						UpdateSatVib(input.Position.X, input.Position.Y)
 					elseif activeSlider == "Hue" then
